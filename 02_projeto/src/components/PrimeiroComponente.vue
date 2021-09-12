a93026949595ef8a88b503208bcd38b7c30b7b9f
add-flav1s <aflavia17_@hotmail.com>
2021-09-12 13:01:25 -0300

    Aprendendo sobre ciclo de vida do vue.

---
+++ 02_projeto/src/components/PrimeiroComponente.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Olá Vue!</h1>
+    <p>Me nome é: {{ nome }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PrimeiroComponente",
+  data() {
+    return {
+      nome: "Ana Flávia",
+    };
+  },
+};
+</script>
