ca5ba5679d67e221c8c811a65303f267a665ef97
add-flav1s <aflavia17_@hotmail.com>
2021-09-28 21:55:54 -0300

    Criando um simples menu com vuejs.

---
+++ navigation-sidebar/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+        <header>
+            <NavBar />
+        </header>
+    </div>
+</template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+
+export default {
+    name: "App",
+    components: {
+        NavBar,
+    },
+};
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-family: "montserrat", sans-serif;
+}
+
+header {
+    width: 100vw;
+    background-color: #455a64;
+    padding: 20px;
+}
+</style>
---
+++ navigation-sidebar/src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav>
+        <div class="menu-item"><a href="#">home</a></div>
+        <div class="menu-item"><a href="#">about</a></div>
+        <div class="menu-item"><a href="#">portfolio</a></div>
+        <div class="menu-item"><a href="#">contact</a></div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "navbar",
+};
+</script>
+
+<style>
+nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.menu-item {
+    color: #fff;
+    padding: 10px 50px;
+    position: relative;
+    text-align: center;
+    border-bottom: 3px solid transparent;
+    display: flex;
+    transition: 0.4s;
+}
+
+.menu-item.active,
+.menu-item:hover {
+    background-color: #78909c;
+    border-bottom-color: #f06292;
+}
+
+.menu-item a {
+    color: inherit;
+    text-decoration: none;
+}
+</style>
