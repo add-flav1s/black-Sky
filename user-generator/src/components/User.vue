f3fa40e77ab93115d68da483809e131c2fae58e7
add-flav1s <aflavia17_@hotmail.com>
2021-10-02 15:31:00 -0300

    Iniciando o projeto de user random, criando o esqueleto do projeto.

---
+++ user-generator/src/components/User.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <img v-bind:src="picture" alt="" />
+        <h1>{{ firstName }} {{ lastName }}</h1>
+        <h3>Email: {{ email }}</h3>
+        <button>Get Random User</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "User",
+    data() {
+        return {
+            firstName: "John",
+            lastName: "Doe",
+            email: "randomuser3@email.com",
+            gender: "female",
+            picture: "https://randomuser.me/api/portraits/med/men/39.jpg",
+        };
+    },
+};
+</script>
